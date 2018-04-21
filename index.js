97e95ddaa5a90d776a936cd7e37d74aae848eed7
ssterian@hotmail.com <ssterian@hotmail.com>
2018-04-21 15:33:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift();
+}
+function appendKitten(name)
+{
+  var arr = [kittens...,name];
+  retrun arr;
+}
+function appendKitten(name)
+{
+  var arr = [kittens...,name];
+  retrun arr;
+}
+function prependKitten(name)
+{
+  var arr = [name,...kittens];
+  retrun arr;
+} 
+function removeLastKitten()
+{
+  var arr = kittens.slice(0,kittens.length-1);
+  retrun arr;
+} 
+function removeFirstKitten()
+{
+  var arr = kittens.slice(1,kittens.length-1);
+  retrun arr;
+} 
+
+ 
